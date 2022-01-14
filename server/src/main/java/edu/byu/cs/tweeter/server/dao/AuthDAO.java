145e0740a13ce82018bda8db9c029f080bd5da76
Parker King <kingparker.a@gmail.com>
2022-01-14 00:13:16 -0700

    Updated AuthToken

--- server/src/main/java/edu/byu/cs/tweeter/server/dao/AuthDAO.java
+++ server/src/main/java/edu/byu/cs/tweeter/server/dao/AuthDAO.java
@@ -3,9 +3,10 @@ package edu.byu.cs.tweeter.server.dao;
 import edu.byu.cs.tweeter.model.domain.AuthToken;
 
 public class AuthDAO extends DAO {
+    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
     public boolean isValidAuthToken(String alias, AuthToken authToken) {
         // TODO: Change to actual logic instead of dummy data
-        return !alias.equals("@BadAuth");
+        return authToken != null && !alias.equals("@BadAuth");
     }
 
     public AuthToken generateLoginToken(String alias) {
