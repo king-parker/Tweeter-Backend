49355de33e869fc315af3bc2114317cb2668a737
Parker King <kingparker.a@gmail.com>
2022-01-16 16:18:48 -0700

    Prep shared model for real database

--- server/src/main/java/edu/byu/cs/tweeter/server/dao/AuthDAO.java
+++ server/src/main/java/edu/byu/cs/tweeter/server/dao/AuthDAO.java
@@ -1,8 +1,11 @@
 package edu.byu.cs.tweeter.server.dao;
 
 import edu.byu.cs.tweeter.model.domain.AuthToken;
+import edu.byu.cs.tweeter.model.util.Timestamp;
 
 public interface AuthDAO {
+    long TOKEN_TIMEOUT = Timestamp.getDuration(0,10, 0);
+
     boolean isValidAuthToken(String alias, AuthToken authToken);
     AuthToken generateLoginToken(String alias);
     void endUserSession(String alias, AuthToken authToken);
